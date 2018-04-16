c6fa526cef419fbca0fd0fa96aba5cdc4761e742
Lasith-Niro <lasithniro.online@gmail.com>
2018-04-17 05:16:12 +0530

    first commit

---
+++ backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/783A8DB93A8D74C2/TOPEND/Testing-Area/JWT/backend/env/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
